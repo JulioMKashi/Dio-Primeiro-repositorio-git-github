8dce67124d95691f91edece4400881c49d4cc795
Júlio Maruyama Kashiwaya <julio.kashi@hotmail.com>
2021-11-10 14:19:01 -0300

    Estudo sobre HTTP e SSL

--- EstudoHttpSsl/src/main/java/com/httpexamples/getRequest.java
+++ EstudoHttpSsl/src/main/java/com/httpexamples/getRequest.java
@@ -1,2 +1,34 @@
-package com.httpexamples;public class getRequest {
-}
+package com.httpexamples;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class getRequest {
+    public static final String URL_GET = "https://jsonplaceholder.typicode.com/posts";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // cliente HTTP
+        HttpClient client = HttpClient.newHttpClient();
+
+        // criar a requisicao
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_GET))
+                .build();
+
+        // enviando uma solicitacao
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //imprimir o conteudo recebido
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }
+
+    }
